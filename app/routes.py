03bb8ad666208b7a69e1a70c179892608fab6207
sebmengom <Sebmengom@gmail.com>
2025-03-22 14:16:24 -0600

    Progreso

--- app/routes.py
+++ app/routes.py
@@ -4,12 +4,18 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    with open('app/static/data/intro.txt','r')as f:
+        intro = f.read()
+    return render_template('index.html', intro = intro)
 
 @app.route('/autogestion')
 def autogestion():
-    return render_template('autogestion.html')
+    with open('app/static/data/autogestion.txt','r') as f:
+        autogestion = f.read()
+    return render_template('autogestion.html', autogestion = autogestion)
 
 @app.route('/colaboracion')
 def colaboracion():
-    return render_template('colaboracion.html')
+    with open('app/static/data/colaboracion.txt', 'r')as f:
+        colaboracion = f.read()
+    return render_template('colaboracion.html', colaboracion = colaboracion)
