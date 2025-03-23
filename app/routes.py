17e553ff93771cb6d27fd97c7e48cb49dced181f
sebmengom <Sebmengom@gmail.com>
2025-03-22 23:00:18 -0600

    Day work
    CSS and HTML again

--- app/routes.py
+++ app/routes.py
@@ -4,18 +4,12 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    with open('app/static/data/intro.txt','r')as f:
-        intro = f.read()
-    return render_template('index.html', intro = intro)
+    return render_template('index.html')
 
 @app.route('/autogestion')
 def autogestion():
-    with open('app/static/data/autogestion.txt','r') as f:
-        autogestion = f.read()
-    return render_template('autogestion.html', autogestion = autogestion)
+    return render_template('autogestion.html')
 
 @app.route('/colaboracion')
 def colaboracion():
-    with open('app/static/data/colaboracion.txt', 'r')as f:
-        colaboracion = f.read()
-    return render_template('colaboracion.html', colaboracion = colaboracion)
+    return render_template('colaboracion.html')
